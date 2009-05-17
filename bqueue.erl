eee28a5c2d5e0c1a2b0177e3eb7fc8e0ac7b51cd
Barry Mitchelson <barry.mitchelson@gmail.com>
2009-05-17 01:04:48 +0100

    Added bqueue

---
+++ bqueue.erl
@@ -0,0 +1,30 @@
+-module(bqueue).
+-compile(export_all).
+
+start(ServerName) ->
+  register(ServerName, spawn(fun() -> loop([]) end)).
+
+loop(Queue) ->
+  receive
+    {From, {add, Data}} -> 
+     NewQueue = [Data|Queue],
+     io:format("Queue: ~p -> ~p~n",[Queue,NewQueue]),
+     From ! {ok, NewQueue},
+     loop(NewQueue);
+   {From, reserve} when Queue =:= [] ->
+     io:format("queue empty"),
+     loop([]);
+   {From, reserve} ->
+     [H|T] = lists:reverse(Queue),
+     From ! {ok, H},
+     io:format("Queue: ~p -> ~p (Returned ~p)~n",[Queue,T,H]),
+     loop(lists:reverse(T));
+   {From, die} ->
+     From ! {bye},
+     io:format("Exiting~n");
+   Other ->
+     io:format("Huh?~n"),
+     loop(Queue)
+end.
+
+
