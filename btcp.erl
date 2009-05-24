54853fcea305c237d39fe90ab20c9a5f65f07e43
Barry Mitchelson <barry.mitchelson@gmail.com>
2009-05-24 18:04:25 +0100

    Renamed btcps

--- btcp.erl
+++ btcp.erl
@@ -1,45 +1,41 @@
--module(btcp).
+-module(btcp2).
 -compile(export_all).
 
 -define(LISTEN_PORT, 11300).
--define(TCP_OPTS, [binary, {packet, raw}, {nodelay, true}, {reuseaddr, true}, {active, once}]).
+-define(TCP_OPTS, [list, {packet, 0}, {active, false}, {reuseaddr, true}]).
 
-start_server() -> 
-  % bqueue:start(bqueue_server),
-  case gen_tcp:listen(?LISTEN_PORT, ?TCP_OPTS) of
-    {ok, Listen} -> 
-      Pid = spawn(?MODULE, connect, [Listen]),
-      io:format("~p Server Started. ~p~n", [erlang:localtime(), Pid]);
+start() ->
+  start(?LISTEN_PORT).
+
+start(Port) ->
+  io:format("Starting~n"),
+  case gen_tcp:listen(Port, [list, {packet, 0}, {active, false}, {reuseaddr, true}]) of
+    {ok, ListeningSocket} ->
+      io:format("~p Server started ~p~n", [?MODULE, erlang:localtime()]),
+      accept_connection(ListeningSocket);
     Error -> 
-      io:format("start_server Error: ~p~n", [Error])
+      io:format("start_server error: ~p~n", [Error])
   end.
 
-connect(Listen) -> 
-  io:format("connect started ~p~n", [self()]),
-  io:format("Listen : ~p~n", [Listen]),
-  case gen_tcp:accept(Listen) of
+accept_connection(ListeningSocket) ->
+  case gen_tcp:accept(ListeningSocket) of
     {ok, Socket} ->
-      io:format("ok~n"),
-      inet:setopts(Socket, ?TCP_OPTS),
-      spawn(fun() -> connect(Listen) end),
-      recv_loop(Socket),
-      gen_tcp:close(Socket),
-      io:format("Closed~n");
-    {error, Reason} -> 
-      io:format("connect Error: ~p~n", [Reason])
+      spawn_link(?MODULE, responder_loop, [Socket]),
+      accept_connection(ListeningSocket);
+    {error, Reason} ->
+      io:format("acception_connection error: ~p~n", [Reason])
   end.
 
-recv_loop(Socket) -> 
-  io:format("recv_loop started ~p~n", [self()]),
-  inet:setopts(Socket, [{active, once}]),
-  receive
-    {tcp, Socket, <<"kill\r\n">>} ->
-      bqueue_server ! {self(), die};
-    {tcp, Socket, Data} -> 
-      io:format("~p ~p ~p~n", [inet:peername(Socket), erlang:localtime(), Data]),
-      Response = "Received: " ++ binary_to_list(Data) ++ "\r\n",
-      gen_tcp:send(Socket, Response),
-      recv_loop(Socket);
-    {tcp_closed, Socket} ->
-      io:format("~p Client Disconnected.~n", [erlang:localtime()])
+responder_loop(Socket) ->
+  case gen_tcp:recv(Socket, 0) of
+    {ok, "kill\r\n"} ->
+      gen_tcp:send(Socket, "Exiting\r\n"),
+      gen_tcp:close(Socket),
+      init:stop();
+    {ok, Other} ->
+      gen_tcp:send(Socket, Other),
+      responder_loop(Socket);
+    {error, closed} ->
+      ok
   end.
+
