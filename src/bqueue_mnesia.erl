8778de6077ce0baba2cf45a45c07ee383d31b83f
Barry Mitchelson <barry.mitchelson@gmail.com>
2009-06-07 18:45:52 +0100

    More mnesia fiddling.

--- src/bqueue_mnesia.erl
+++ src/bqueue_mnesia.erl
@@ -1,7 +1,9 @@
 -module(bqueue_mnesia).
 -compile(export_all).
 
--record(job, {id, queue = default, body}).
+-include_lib("stdlib/include/qlc.hrl").
+
+-record(job, { id, queue = default, body, due = 0, priority = 3000 }).
 
 % Eshell V5.7.1  (abort with ^G)
 % 1> rr('src/bqueue_mnesia').
@@ -17,17 +19,28 @@ start() ->
   mnesia:wait_for_tables([job], 5000),
   io:format("Ready~n").
 
-add_job(Body) ->
-  Job = #job{ id = {now(), node()}, body = Body},
+add_job(Body, Priority) ->
+  Job = #job{ id = {now(), node()}, body = Body, priority = Priority},
   F = fun() -> mnesia:write(Job) end,
   Result = mnesia:transaction(F),
   Result.
 
-% get_job() ->
-%   get_job(default).
-% 
-% get_job(Queue) ->
-%   do
+get_job() ->
+  get_job("default").
+
+get_job(QueueName) ->
+  Q1 = qlc:q([ X || X <- mnesia:table(job) ]),
+  Q2 = qlc:sort(Q1, {order, fun(Job1, Job2) -> Job1#job.priority < Job2#job.priority end}),
+  H = do(Q2),
+  C = qlc:cursor(H),
+  R = qlc:next_answers(C,1),
+  qlc:delete_cursor(C),
+  R.
+
+
+all_jobs() ->
+  Q = qlc:q([X || X <- mnesia:table(job)]),
+  do(Q).
 
 
 
@@ -49,3 +62,14 @@ create_table() ->
 reset() ->
   delete_table(),
   create_table().
+
+dummy() ->
+  add_job("one", 5),
+  add_job("two", 2),
+  add_job("three", 1),
+  add_job("four", 7),
+  add_job("five", 100),
+  add_job("six", 2).
+
+dummy(Amount) ->
+  [add_job("foo", X) || X <- lists:seq(1,Amount,1)].
