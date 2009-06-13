bd8a2319f385b4463a23794c09dbc05033ca6db7
Barry Mitchelson <barry.mitchelson@gmail.com>
2009-06-13 19:02:00 +0100

    Added queue names

--- src/bqueue_m.erl
+++ src/bqueue_m.erl
@@ -2,16 +2,16 @@
 -compile(export_all).
 -include_lib("stdlib/include/qlc.hrl").
 
--record(job, { id, body }).
+-record(job, { id, body, queue }).
 
 start() ->
   ok = mnesia:start(),
   ok = mnesia:wait_for_tables([job], 5000),
   io:format("Started~n").
 
-add_job(Body, Priority, Delay) ->
+add_job(Body, Priority, Delay, QueueName) ->
   Id = {Priority, date_utils:timestamp() + Delay, {now(), node()}},
-  Job = #job{id = Id, body = Body},
+  Job = #job{id = Id, body = Body, queue = QueueName},
   F = fun() ->
       mnesia:write(Job)
   end,
@@ -21,16 +21,20 @@ add_job(Body, Priority, Delay) ->
 all_jobs() ->
   utils:mnesia_do(qlc:q([X || X <- mnesia:table(job)])).
 
-get_job() ->
-  F = fun() -> 
-      case mnesia:read(job, mnesia:first(job)) of
-        [Job] ->
-          mnesia:delete({job, Job#job.id}),
+get_job(QueueName) ->
+  F = fun() ->
+      MatchHead = #job{id='$1', body='_', queue='$2'},
+      Guard = {'=:=','$2',QueueName},
+      Result = '$1',
+      case mnesia:select(job, [{MatchHead, [Guard], [Result]}],1,read) of
+        {[Key], _} -> 
+          Job = mnesia:read(job, Key),
+          mnesia:delete({job, Key}),
           Job;
-        [] ->
+        '$end_of_table' ->
           empty;
         Other ->
-          io:format("Error : ~p~n", [Other])
+          io:format("Error: ~p~n", [Other])
       end
   end,
   {atomic, Response} = mnesia:transaction(F),
@@ -43,13 +47,16 @@ bootstrap() ->
   mnesia:create_table(job, [{type, ordered_set}, {disc_copies, [node()]}, {attributes, record_info(fields, job)}]).
 
 fill(Amount) ->
-  [add_job(X, X, 100) || X <- lists:seq(1, Amount)].
+  fill(Amount, "default").
 
-grab_all() ->
-  grab(mnesia:table_info(job, size)).
+fill(Amount, QueueName) ->
+  [add_job(X, X, 100, QueueName) || X <- lists:seq(1, Amount)].
 
-grab(Amount) ->
-  case get_job() of
+grab_all(QueueName) ->
+  grab(mnesia:table_info(job, size), QueueName).
+
+grab(Amount, QueueName) ->
+  case get_job(QueueName) of
     empty ->
       io:format("Finished~n");
     Job ->
@@ -58,7 +65,7 @@ grab(Amount) ->
         1 -> 
           io:format("Done~n");
         _ ->
-        grab(Amount - 1)
+        grab(Amount - 1, QueueName)
     end
   end.
 
