cc14eaad43d6170bb420c2fa1eceb84473ae8b77
Barry Mitchelson <barry.mitchelson@gmail.com>
2009-06-13 12:56:29 +0100

    Added fill, grab and grab_all

--- src/bqueue_m.erl
+++ src/bqueue_m.erl
@@ -23,14 +23,42 @@ all_jobs() ->
 
 get_job() ->
   F = fun() -> 
-      [Job] = mnesia:read(job, mnesia:first(job)),
-      mnesia:delete({job, Job#job.id}),
-      Job
+      case mnesia:read(job, mnesia:first(job)) of
+        [Job] ->
+          mnesia:delete({job, Job#job.id}),
+          Job;
+        [] ->
+          empty;
+        Other ->
+          io:format("Error : ~p~n", [Other])
+      end
   end,
-  mnesia:transaction(F).
+  {atomic, Response} = mnesia:transaction(F),
+  Response.
 
 
 bootstrap() ->
   mnesia:start(),
   mnesia:delete_table(job),
   mnesia:create_table(job, [{type, ordered_set}, {disc_copies, [node()]}, {attributes, record_info(fields, job)}]).
+
+fill(Amount) ->
+  [add_job(X, X, 100) || X <- lists:seq(1, Amount)].
+
+grab_all() ->
+  grab(mnesia:table_info(job, size)).
+
+grab(Amount) ->
+  case get_job() of
+    empty ->
+      io:format("Finished~n");
+    Job ->
+      io:format("Got ~p : ~p~n", [Amount, Job#job.body]),
+      case Amount of
+        1 -> 
+          io:format("Done~n");
+        _ ->
+        grab(Amount - 1)
+    end
+  end.
+
